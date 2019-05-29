efaba5a88288dc2202a7dac0505a7f34a4450591
Leonard Krause <leoek2011@gmail.com>
2019-05-29 17:08:54 +0200

    basic async native addon setup

--- binding.gyp
+++ binding.gyp
@@ -2,7 +2,7 @@
   "targets": [
     {
       "target_name": "nativeaddon",
-      "sources": [ "src/nativeaddon.cc" ],
+      "sources": [ "src/nativeaddon.cc", "src/pi_est.cc", "src/async.cc" ],
       "include_dirs": [ "<!(node -e \"require('nan')\")" ]
     }
   ]
--- src/app.js
+++ src/app.js
@@ -4,4 +4,44 @@ const addon = bindings("nativeaddon");
 
 console.log("test");
 
-addon.createSolution()
+function printResult(type, pi, ms) {
+  console.log(type, 'method:');
+  console.log('\tπ ≈ ' + pi +
+              ' (' + Math.abs(pi - Math.PI) + ' away from actual)');
+  console.log('\tTook ' + ms + 'ms');
+  console.log();
+}
+
+var calculations = 10000000000;
+
+const runAsync = () => {
+  var batches = 16;
+  var ended = 0;
+  var total = 0;
+  var start = Date.now();
+
+  const done = (err, result) => {
+    total += result;
+
+    // have all the batches finished executing?
+    if (++ended === batches) {
+      printResult('Async', total / batches, Date.now() - start);
+    }
+  }
+
+  for (var i = 0; i < batches; i++) {
+    addon.createSolution(calculations / batches, done);
+  }
+
+}
+
+runAsync();
+
+let flag = true;
+let counter = 0;
+
+setTimeout(() => { flag = false; console.log("done")}, 10000);
+
+while(flag){
+  counter = counter + Math.log(Math.sqrt(counter));
+}
