c3f99b681334425a960810fe51063818f52a5c02
Leonard <leoek2011@gmail.com>
2020-06-12 17:32:04 +0200

    :sparkles: add paramils parameter parser

--- src/lib/parameterParser.js
+++ src/lib/parameterParser.js
@@ -1,4 +1,64 @@
+import set from "lodash/set";
 import { INSTANCE_TYPE } from "../config";
+
+const indexParser = parameters => {
+  process.argv.forEach((val, i) => {
+    // 0 and 1 are node and the js filename
+    if (i === 3) {
+      parameters.instanceName = val;
+    } else if (i === 4) {
+      parameters.instanceType = val;
+    } else if (i === 5) {
+      parameters.agents = parseInt(val);
+    } else if (i === 6) {
+      parameters.solutionCountTarget = parseInt(val);
+    } else if (i === 7) {
+      parameters.n = parseInt(val);
+    } else if (i === 8) {
+      parameters.agentOptions.maxPersonalBest = parseInt(val);
+    } else if (i === 9) {
+      parameters.agentOptions.maxGlobalBest = parseInt(val);
+    } else if (i === 10) {
+      parameters.agentOptions.pBestPopulationWeight = parseInt(val);
+    } else if (i === 11) {
+      parameters.agentOptions.gBestPopulationWeight = parseInt(val);
+    } else if (i === 12) {
+      parameters.agentOptions.rndWeight = parseInt(val);
+    }
+  });
+  return parameters;
+};
+
+const paramilsParser = parameters => {
+  const paramNames = Object.keys(parameters);
+  const agentOptionParamNames = Object.keys(parameters.agentOptions);
+  let lastParamPath;
+  process.argv.forEach((val, i) => {
+    // 0 and 1 are node and the js filename
+    if (i === 3) {
+      parameters.instanceName = val;
+    } else if (i === 4) {
+      parameters.instanceType = val;
+    } else if (i === 5) {
+      parameters.cutoff_time = val;
+    } else if (i === 6) {
+      parameters.cutoff_length = val;
+    } else if (i === 7) {
+      parameters.seed = val;
+    } else if (val.startsWith("-") && paramNames.includes(val.substr(1))) {
+      lastParamPath = val.substr(1);
+    } else if (
+      val.startsWith("-") &&
+      agentOptionParamNames.includes(val.substr(1))
+    ) {
+      lastParamPath = `agentOptions.${val.substr(1)}`;
+    } else if (lastParamPath) {
+      set(parameters, lastParamPath, parseInt(val));
+      lastParamPath = null;
+    }
+  });
+  return parameters;
+};
 
 export const getParametersFromArgs = () => {
   const parameters = {
@@ -16,31 +76,11 @@ export const getParametersFromArgs = () => {
     },
     seed: -1
   };
-  process.argv.forEach((val, i) => {
-    // 0 and 1 are node and the js filename
-    if (i === 2) {
-      parameters.instanceName = val;
-    } else if (i === 3) {
-      parameters.instanceType = val;
-    } else if (i === 4) {
-      parameters.agents = parseInt(val);
-    } else if (i === 5) {
-      parameters.solutionCountTarget = parseInt(val);
-    } else if (i === 6) {
-      parameters.n = parseInt(val);
-    } else if (i === 7) {
-      parameters.agentOptions.maxPersonalBest = parseInt(val);
-    } else if (i === 8) {
-      parameters.agentOptions.maxGlobalBest = parseInt(val);
-    } else if (i === 9) {
-      parameters.agentOptions.pBestPopulationWeight = parseInt(val);
-    } else if (i === 10) {
-      parameters.agentOptions.gBestPopulationWeight = parseInt(val);
-    } else if (i === 11) {
-      parameters.agentOptions.rndWeight = parseInt(val);
-    }
-  });
-  return parameters;
+  // 0 and 1 are node and the js filename
+  if (process.argv[2] === "paramils") {
+    return paramilsParser(parameters);
+  }
+  return indexParser(parameters);
 };
 
 export default getParametersFromArgs;
